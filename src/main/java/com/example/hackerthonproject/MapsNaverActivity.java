10cf91672284e0c44fc6cea95ac9c5b37021bbcc
Garfield <iju1633@gmail.com>
2021-08-20 21:12:59 +0900

    화면 구성 완료 + qr 코드 찍기(코드 찍고 아무것도 일어나지 않음)

--- src/main/java/com/example/hackerthonproject/MapsNaverActivity.java
+++ src/main/java/com/example/hackerthonproject/MapsNaverActivity.java
@@ -10,12 +10,17 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.journeyapps.barcodescanner.CaptureActivity;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.Marker;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
 
@@ -24,9 +29,9 @@ public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
     Button home, QR_Scan, myPage;
 
 
-//    IntentIntegrator integrator;
-//    private int point = 1000;
-//    private int bonus = 1000;
+    IntentIntegrator integrator;
+    private int point = 1000;
+    private int bonus = 1000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,10 +58,47 @@ public class MapsNaverActivity extends Activity implements OnMapReadyCallback {
     }
 
     public void QR_Scan(View view){
+        integrator = new IntentIntegrator(this);
 
+        integrator.setPrompt("QR 코드를 사각형 안에 위치 시켜주세요");
+
+        //QR 코드 인식 시에 삐- 소리가 나게 할것인지 여부
+        integrator.setBeepEnabled(true);
+
+        integrator.setBarcodeImageEnabled(true);
+
+        integrator.setCaptureActivity(CaptureActivity.class);
+
+        //스캐너 시작 메소드
+        integrator.initiateScan();
     }
 
     public void myPage(View view){
+        mapInfo_MyPage.setText(" ");
+    }
 
+    public int getPoint() {
+        return point;
+    }
+
+    // 적립될 포인트
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public void addPoint() {
+        this.point += getBonus();
+    }
+
+    public String printInfo(){
+        return "  적립된 Point : " + getBonus() + "원";
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
     }
 }
